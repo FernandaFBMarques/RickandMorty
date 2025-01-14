ee1e266b37933511130e2d7761af26370885bb54
Maria Fernanda de Freitas Barbosa Marques <fernandafbmarques@gmail.com>
2025-01-14 10:30:40 -0300

    adjust in the hilt dependecies

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -30,11 +30,11 @@ android {
         }
     }
     compileOptions {
-        sourceCompatibility = JavaVersion.VERSION_1_8
-        targetCompatibility = JavaVersion.VERSION_1_8
+        sourceCompatibility = JavaVersion.VERSION_17
+        targetCompatibility = JavaVersion.VERSION_17
     }
     kotlinOptions {
-        jvmTarget = "11"
+        jvmTarget = "17"
     }
     buildFeatures {
         compose = true
@@ -71,5 +71,5 @@ dependencies {
 }
 
 kapt {
-    correctErrorTypes true
+    correctErrorTypes = true
 }
