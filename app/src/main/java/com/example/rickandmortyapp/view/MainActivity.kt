4960b39736d4d91d08f12ce53acc4e052af1b9e3
Maria Fernanda de Freitas Barbosa Marques <fernandafbmarques@gmail.com>
2025-02-17 19:37:51 -0300

    - demo for pagination

--- app/src/main/java/com/example/rickandmortyapp/view/MainActivity.kt
+++ app/src/main/java/com/example/rickandmortyapp/view/MainActivity.kt
@@ -2,27 +2,58 @@ package com.example.rickandmortyapp.view
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.util.Log
 import androidx.recyclerview.widget.LinearLayoutManager
 import com.example.rickandmortyapp.viewmodel.CharacterAdapter
 import com.example.rickandmortyapp.databinding.ActivityMainBinding
 import com.example.rickandmortyapp.viewmodel.FetchCharacters
-
+import androidx.core.widget.NestedScrollView
+import androidx.databinding.DataBindingUtil
+import com.example.rickandmortyapp.R
 
 class MainActivity : AppCompatActivity() {
     private lateinit var binding: ActivityMainBinding
+    private var currentPage = 1
     private val characterAdapter: CharacterAdapter by lazy {
         CharacterAdapter()
     }
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        binding = ActivityMainBinding.inflate(layoutInflater)
-        setContentView(binding.root)
-        FetchCharacters.fetchCharacters{ characterAdapter.submitList(it) }
-        binding.rvRecycler
-            .apply{
-                layoutManager= LinearLayoutManager(this@MainActivity)
-                adapter = characterAdapter}
+
+        binding = DataBindingUtil.setContentView(this, R.layout.activity_main)
+
+
+        //binding = ActivityMainBinding.inflate(layoutInflater)
+        //setContentView(binding.root)
+
+        initUI()
 
     }
+    fun initUI(){
+
+        FetchCharacters.fetchCharacters(currentPage){ characterAdapter.submitList(it) }
+
+        binding.apply {
+            charactersView.setOnScrollChangeListener(NestedScrollView.
+            OnScrollChangeListener { v, _, scrollY, _, oldScrollY ->
+                Log.i("yaya", "is here? scrollY: $scrollY oldScrollY: $oldScrollY")
+                if((scrollY > oldScrollY)&&(!v.canScrollVertically(1))){
+
+                        currentPage++
+                        FetchCharacters.fetchCharacters(currentPage){ characterAdapter.submitList(it) }
+
+                }
+
+            })
+        }
+
+        binding
+            .apply {
+                rvRecycler.layoutManager= LinearLayoutManager(this@MainActivity)
+                rvRecycler.adapter = characterAdapter
+            }
+
+    }
+
 }
---
+++ app/src/main/java/com/example/rickandmortyapp/viewmodel/PaginationScrollListener.kt
@@ -0,0 +1,16 @@
+package com.example.rickandmortyapp.viewmodel
+
+class PaginationScrollListener {
+
+    private val characterAdapter: CharacterAdapter by lazy {
+        CharacterAdapter()
+    }
+
+    fun loadData(page:Int) {
+        val isLoading = true
+
+
+
+    }
+
+}
